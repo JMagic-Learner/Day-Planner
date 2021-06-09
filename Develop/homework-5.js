e32b574b59c89c46bf95b1d9f52a8a970942e27c
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-06-08 20:23:41 -0700

    Oh my god this is a huge change from initial. I made it so that the time-block element only gets appended up to a certain point. The code then stops at the current hour, then continues to implement new code at future. Furthremore, looking into the CSS reveals that textarea is needed to implement a text functionality for events. Right now I am looking at ways to overlap the text area

--- Develop/homework-5.js
+++ Develop/homework-5.js
@@ -1,12 +1,19 @@
-var example = moment("2021-03-03");
-var currentDayValue = moment();
-var textExample = document.querySelector("#workplan");
-var presentSchedule = document.querySelector(".present");
-var pastClick = document.querySelector(".past");
 
-$(".hour").text(example);
-$(".present").text(currentDayValue);
-$("#currentDay").text(currentDayValue);
+var currentDayValue = moment();
+var onlyMonthDayYear = moment().format("dddd MMMM Do YYYY hh:mm:ss");
+var textExample = document.querySelector("#workplan");
+var presentSchedule = document.querySelectorAll(".present");
+var pastClick = document.querySelectorAll(".past");
+var futureClick = document.querySelectorAll(".future");
+var hoursOnly = 12;
+//moment().format("HH");
+var timeblockClick = document.querySelectorAll(".time-block");
+
+console.log(hoursOnly);
+
+
+var hourcount = 8;
+
 
 
     
@@ -19,5 +26,70 @@ $("#currentDay").text(currentDayValue);
         $("#description").text(entryDate);
     }
 
-pastClick.addEventListener('click', entryALL );
-presentSchedule.addEventListener('click', entryALL);
+function determinePastBlock() { 
+        var target = document.querySelector('.container').innerHTML += 
+        '<div class = "time-block" id = "Timeblock"><div class = "row past hour" id = "workplan"> </div> <label> Event </label><textarea class = "past" rows="2" cols="90"> </textarea></div>';
+        $(".hour").text(onlyMonthDayYear);
+
+        console.log ("the function has created one time block");    
+}
+
+function determineCurrentBlock(){
+    var target = document.querySelector('.container').innerHTML += 
+        '<div class = "time-block" id = "Timeblock"><div class = "row present hour" id = "workplan"></div></div>';
+        $(".hour").text(onlyMonthDayYear);
+
+        console.log ("the function has created one time block");   
+}
+
+function determineFutureBlock() {
+    var target = document.querySelector('.container').innerHTML += 
+    '<div class = "time-block" id = "Timeblock"><div class = "row future hour" id = "workplan"></div></div>';
+    $(".hour").text(onlyMonthDayYear);
+
+    console.log ("the function has created one time block");   
+}
+
+
+
+
+function repeatNine() {
+
+    if (hoursOnly >= 8 && hoursOnly <= 17) {
+        for (var i = 8; i < hoursOnly; i++){
+            hourcount++;
+            console.log("The function is adding HTML elements up to the current hour");
+            determinePastBlock();
+            
+        }
+        determineCurrentBlock();
+
+        var remainder = 17 - hoursOnly
+        for (var k = 0; k < remainder ; k++){
+            hourcount++;
+            console.log("The function is now adding elements beyond the current hour");
+            determineFutureBlock();
+        }
+
+
+
+    } else if (hoursOnly < 8 || hoursOnly > 17) {
+        for (var i = 0; i < 9; i++){
+            hourcount++;
+            console.log('The function is adding only 9 sets of HTML elements becuase the current hour is past standard business hours');
+            determinePastBlock();
+        }    
+    }
+}
+
+
+
+$(".hour").text(onlyMonthDayYear);
+$(".present").text(currentDayValue);
+$("#currentDay").text(currentDayValue);
+$(".future").text(onlyMonthDayYear);
+   /* for (var i = 0; i <= 8; i++) {
+        var hourTimeBlock = document.getElementById('#' + i);
+        */
+
+repeatNine();
