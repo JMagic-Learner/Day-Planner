9d7a1c8a2177090f008eac585cf74baa6e818d96
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-06-10 00:02:23 -0700

    save breaks the code, edit it all out

--- Develop/homework-5.js
+++ Develop/homework-5.js
@@ -1,44 +1,77 @@
 
 var currentDayValue = moment();
 var onlyMonthDayYear = moment().format("dddd MMMM Do YYYY hh:mm:ss");
-var textExample = document.querySelector("#workplan");
+
 var presentSchedule = document.querySelectorAll(".present");
 
-var hoursOnly = 15;
-//moment().format("HH");
+var hoursOnly = 13;
+
+moment().format("HH");
 var hourcount = 8;
 
 console.log(hoursOnly);
 var value = 7;
 
-
-
-
+    /* var local0800value = localStorage.getItem("0800");
+    local0800value.textContent = browser0800value;
+    
+    
+    
+    var local0900value = localStorage.getItem("0900");
+    document.getElementsByClassName("0900").value = browser0900value; 
 
     
     
- function  entryALL()  {
-        var entry = prompt("Enter your event name.");
-        var entryDate = prompt("Enter your schedule time.");
-        var descriptionValue = document.getElementById("#description");
-        console.log(entry + entryDate);
-        $("#description").text(entryDate);
-    }
+    var local1000value = localStorage.getItem("1000");
+    document.getElementsByClassName("1000").value = browser1000value;
+    
+    
+    
+    var local1100value = localStorage.getItem("1100");
+    document.getElementsByClassName("1100").value = browser1100value;
+   
+   
+    var local1200value = localStorage.getItem("1200");
+    document.getElementsByClassName("1200").value = browser1200value;
+    
+    
+    var local0100value = localStorage.getItem("0100");
+    document.getElementsByClassName("0100").value = browser0100value;
+
+    
+    
+    var browser0200value = document.getElementsByClassName("0200").value;
+    document.getElementsByClassName("0200").value = browser0200value;
+
+    
+    var browser0300value = document.getElementsByClassName("0300").value;
+    document.getElementsByClassName("0300").value = browser0300value;
+
+    
+    var browser0400value = document.getElementsByClassName("0400").value;
+    document.getElementsByClassName("0400").value = browser0400value;
+
+    
+    var browser0400value = document.getElementsByClassName("0400").value;
+    document.getElementsByClassName("0400").value = browser0400value;
+*/
+
+
+
 
 function determinePastBlock() {        
         var target = document.querySelector('.container').innerHTML += 
-        '<div class = "time-block" id = "Timeblock"><label class = "hour-past"></label><textarea class = "row past"></textarea><button class = "saveBtn"> SAVE </button></div>';
-        $(".hour-past").text("Past");
-        //var storeTEXT = document.getElementById("row").value;
-        //console.log (hourcount);
-        //localStorage.setItem('data', 'storeTEXT'); 
+        '<div class = "time-block" id = "Timeblock"><label class = "hour-past"></label><textarea id = "then" class = "row past"></textarea><button class = "saveBtn"> SAVE </button></div>';
+        
+        
+        
 }
 
 function determineCurrentBlock(){
     var currentHour = moment().format("HH");
     var target = document.querySelector('.container').innerHTML += 
-        '<div class = "time-block" id = "Timeblock"><label class = "hour-present"></label><textarea class = "row present"></textarea><button class = "saveBtn"> SAVE </button></div>';
-        $(".hour-present").text("Now");
+        '<div class = "time-block" id = "Timeblock"><label class = "hour-present"></label><textarea id = "now" class = "row present"></textarea><button class = "saveBtn"> SAVE </button></div>';
+       // $(".hour-present").text("Now");
 
         console.log ("the function has created one time block");   
 }
@@ -46,8 +79,8 @@ function determineCurrentBlock(){
 function determineFutureBlock() {
     var currentHour = moment().format("HH");
     var target = document.querySelector('.container').innerHTML += 
-    '<div class = "time-block" id = "Timeblock"><label class = "hour-future"></label><textarea class = "row future"></textarea><button class = "saveBtn"> SAVE </button></div>';
-        $(".hour-future").text("Next");
+    '<div class = "time-block" id = "Timeblock"><label class = "hour-future"></label><textarea id = "later" class = "row future"></textarea><button class = "saveBtn"> SAVE </button></div>';
+        //$(".hour-future").text("Next");
 
     console.log ("the function has created one time block");   
 }
@@ -92,17 +125,76 @@ function repeatNine() {
 $(".present").text(currentDayValue);
 $("#currentDay").text(currentDayValue);
 $(".future").text(onlyMonthDayYear);
-   /* for (var i = 0; i <= 8; i++) {
-        var hourTimeBlock = document.getElementById('#' + i);
-        */
-var setDown = localStorage.getItem('data');
-$('.hour').text(setDown);
-repeatNine();
+
+        repeatNine();
+        
+       
+        specificTime();
+        save();
 
 var saveACTION = document.querySelector(".saveBtn");
 saveACTION.addEventListener("click", save);
+
+
+
 function save() {
-     localStorage.setItem('data', 'value');
-     var value = localStorage.getItem('data')
-     $('.row').text(value);
+   /*  var browser0800value = document.getElementsByClassName("0800").value;
+    localStorage.setItem("0800", browser0800value);
+    var browser0900value = document.getElementsByClassName("0900").value;
+    localStorage.setItem("0900", browser0900value);
+    var browser1000value = document.getElementsByClassName("1000").value;
+    localStorage.setItem("1000", browser1000value);
+    var browser1100value = document.getElementsByClassName("1100").value;
+    localStorage.setItem("1100", browser1100value);
+    localStorage.setItem("1200", browser1200value);
+    var browser1200value = document.getElementsByClassName("1200").value;
+    localStorage.setItem("0100", browser0100value);
+        var browser0100value = document.getElementsByClassName("0100").value;
+        localStorage.setItem("0200", browser0200value);
+        var local0200value = localStorage.getItem("0200");
+        localStorage.setItem("0300", browser0300value);
+        var local0300value = localStorage.getItem("0300");
+        localStorage.setItem("0400", browser0400value);
+        var local0400value = localStorage.getItem("0400");
+        localStorage.setItem("0400", browser0400value);
+        var local0400value = localStorage.getItem("0400");   
+    
+   */ 
+    
+}
+
+function specificTime() {
+            
+$(document).ready(function(){
+    $("div").children().eq(0).addClass("eight");
+    $("div").children().eq(1).addClass("0800");
+    $(".eight").text("08:00");
+    $("div").next().eq(0).find("label").addClass("nine");
+    $("div").next().eq(0).find("textarea").addClass("0900");
+    $(".nine").text("09:00");
+    $("div").eq(1).next().find("label").addClass("ten");
+    $("div").eq(1).next().find("textarea").addClass("1000");
+    $(".ten").text("10:00");
+    $("div").eq(2).next().find("label").addClass("eleven");
+    $("div").eq(2).next().find("textarea").addClass("1100");
+    $(".eleven").text("11:00");
+    $("div").eq(3).next().find("label").addClass("twelve");
+    $("div").eq(3).next().find("textarea").addClass("1200");
+    $(".twelve").text("12:00");
+    $("div").eq(4).next().find("label").addClass("one");
+    $("div").eq(4).next().find("textarea").addClass("0100");
+    $(".one").text("01:00");
+    $("div").eq(5).next().find("label").addClass("two");
+    $("div").eq(5).next().find("textarea").addClass("0200");
+    $(".two").text("02:00");
+    $("div").eq(6).next().find("label").addClass("three");
+    $("div").eq(6).next().find("textarea").addClass("0300");
+    $(".three").text("03:00");
+    $("div").eq(7).next().find("label").addClass("four");
+    $("div").eq(7).next().find("textarea").addClass("0400");
+    $(".four").text("04:00");
+    $("div").eq(8).next().find("label").addClass("five");
+    $("div").eq(8).next().find("textarea").addClass("0500");
+    $(".five").text("05:00");
+});
 }
